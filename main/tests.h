76295303dc226bd7f79df489c7a97aee55d294bc
FROL-RX480 <vova@frolov.pp.ru>
2018-03-13 17:25:08 +0300

    add test for LT and rect image

--- main/tests.h
+++ main/tests.h
@@ -160,6 +160,7 @@ bool test76_empty_mesh();
 bool test77_save_gbuffer_layers();
 bool test78_material_remap_list1();
 bool test79_material_remap_list2();
+bool test80_lt_rect_image();
 
 bool test90_check_xml_fail_lights();
 bool test91_check_xml_fail_lights();
