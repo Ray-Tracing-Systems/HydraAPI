e33e6d37a615f42e1d0bf2d34c561af651dcc1a1
vsan-msu <vsan@protonmail.com>
2018-05-17 18:45:25 +0300

    start working on subdivision

--- hydra_api/HydraAPI_Geom.cpp
+++ hydra_api/HydraAPI_Geom.cpp
@@ -1247,6 +1247,93 @@ void hrMeshDisplace(HRMeshRef a_mesh)
 }
 
 
+void hrMeshSubdivide(HRMeshRef a_mesh, int a_iterations)
+{
+  HRMesh *pMesh = g_objManager.PtrById(a_mesh);
+  if (pMesh == nullptr)
+  {
+    HrError(L"hrMeshComputeNormals: nullptr input");
+    return;
+  }
+
+  HRMesh::InputTriMesh &mesh = pMesh->m_input;
+
+  const int vertexCount = int(mesh.verticesPos.size() / 4);
+  const int triangleCount = int(mesh.triIndices.size() / 3);
+
+  std::vector<uint32_t> indices;//(mesh.triIndices.size() * 3, 0);
+  std::vector<uint32_t> mat_indices;//(mesh.triIndices.size() * 3 / 3, 0);
+
+  int face_num = 0;
+  for(int i = 0; i < mesh.triIndices.size(); i += 3)
+  {
+    int indA = mesh.triIndices[i + 0];
+    int indB = mesh.triIndices[i + 1];
+    int indC = mesh.triIndices[i + 2];
+
+    float4 A = float4(mesh.verticesPos[indA * 4 + 0], mesh.verticesPos[indA * 4 + 1],
+                      mesh.verticesPos[indA * 4 + 2], mesh.verticesPos[indA * 4 + 3]);
+    float4 B = float4(mesh.verticesPos[indB * 4 + 0], mesh.verticesPos[indB * 4 + 1],
+                      mesh.verticesPos[indB * 4 + 2], mesh.verticesPos[indB * 4 + 3]);
+    float4 C = float4(mesh.verticesPos[indC * 4 + 0], mesh.verticesPos[indC * 4 + 1],
+                      mesh.verticesPos[indC * 4 + 2], mesh.verticesPos[indC * 4 + 3]);
+
+    float4 ANorm = float4(mesh.verticesNorm[indA * 4 + 0], mesh.verticesNorm[indA * 4 + 1],
+                          mesh.verticesNorm[indA * 4 + 2], mesh.verticesNorm[indA * 4 + 3]);
+    float4 BNorm = float4(mesh.verticesNorm[indB * 4 + 0], mesh.verticesNorm[indB * 4 + 1],
+                          mesh.verticesNorm[indB * 4 + 2], mesh.verticesNorm[indB * 4 + 3]);
+    float4 CNorm = float4(mesh.verticesNorm[indC * 4 + 0], mesh.verticesNorm[indC * 4 + 1],
+                          mesh.verticesNorm[indC * 4 + 2], mesh.verticesNorm[indC * 4 + 3]);
+
+    float4 ATan = float4(mesh.verticesTangent[indA * 4 + 0], mesh.verticesTangent[indA * 4 + 1],
+                         mesh.verticesTangent[indA * 4 + 2], mesh.verticesTangent[indA * 4 + 3]);
+    float4 BTan = float4(mesh.verticesTangent[indB * 4 + 0], mesh.verticesTangent[indB * 4 + 1],
+                         mesh.verticesTangent[indB * 4 + 2], mesh.verticesTangent[indB * 4 + 3]);
+    float4 CTan = float4(mesh.verticesTangent[indC * 4 + 0], mesh.verticesTangent[indC * 4 + 1],
+                         mesh.verticesTangent[indC * 4 + 2], mesh.verticesTangent[indC * 4 + 3]);
+
+    float2 Auv = float2(mesh.verticesTexCoord[indA * 2 + 0], mesh.verticesTexCoord[indA * 2 + 1]);
+    float2 Buv = float2(mesh.verticesTexCoord[indB * 2 + 0], mesh.verticesTexCoord[indB * 2 + 1]);
+    float2 Cuv = float2(mesh.verticesTexCoord[indC * 2 + 0], mesh.verticesTexCoord[indC * 2 + 1]);
+
+    float4 P = (A + B + C) / 3.0f;
+    float4 PNorm = (ANorm + BNorm + CNorm) / 3.0f;
+    float4 PTan = (ATan + BTan + CTan) / 3.0f;
+    float2 Puv = (Auv + Buv + Cuv) / 3.0f;
+
+    uint32_t indP = mesh.verticesPos.size() / 4;
+    mesh.verticesPos.push_back(P.x);
+    mesh.verticesPos.push_back(P.y);
+    mesh.verticesPos.push_back(P.z);
+    mesh.verticesPos.push_back(P.w);
+
+    mesh.verticesNorm.push_back(PNorm.x);
+    mesh.verticesNorm.push_back(PNorm.y);
+    mesh.verticesNorm.push_back(PNorm.z);
+    mesh.verticesNorm.push_back(PNorm.w);
+
+    mesh.verticesTangent.push_back(PTan.x);
+    mesh.verticesTangent.push_back(PTan.y);
+    mesh.verticesTangent.push_back(PTan.z);
+    mesh.verticesTangent.push_back(PTan.w);
+
+    mesh.verticesTexCoord.push_back(Puv.x);
+    mesh.verticesTexCoord.push_back(Puv.y);
+
+    indices.push_back(indA); indices.push_back(indB); indices.push_back(indP);
+    mat_indices.push_back(mesh.matIndices[face_num]);
+    indices.push_back(indB); indices.push_back(indC); indices.push_back(indP);
+    mat_indices.push_back(mesh.matIndices[face_num]);
+    indices.push_back(indC); indices.push_back(indA); indices.push_back(indP);
+    mat_indices.push_back(mesh.matIndices[face_num]);
+
+    face_num++;
+  }
+
+  mesh.triIndices = indices;
+  mesh.matIndices = mat_indices;
+}
+
 void doDisplacement(HRMesh *pMesh, const pugi::xml_node &displaceXMLNode, std::vector<uint3> &triangleList)
 {
   HRMesh::InputTriMesh &mesh = pMesh->m_input;
@@ -1401,6 +1488,7 @@ std::wstring HR_PrepocessMeshes(const wchar_t* state_path)
         hrMeshVertexAttribPointer2f(mesh_ref_new, L"texcoord", &verticesTexCoord[0]);
         hrMeshPrimitiveAttribPointer1i(mesh_ref_new, L"mind", (int *) (&matIndices[0]));
         hrMeshAppendTriangles3(mesh_ref_new, int(triIndices.size()), (int *) (&triIndices[0]));
+        //hrMeshSubdivide(mesh_ref_new, 1);
 
         hrMeshDisplace(mesh_ref_new);
         hrMeshClose(mesh_ref_new);
