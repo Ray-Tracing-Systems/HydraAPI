8e7db2ad35e604284e662998d91e94bd2aa2bf37
frol-msu-white-linux <vova@frolov.pp.ru>
2018-07-12 12:21:09 +0300

    first push to compact

--- main/main.cpp
+++ main/main.cpp
@@ -97,7 +97,7 @@ int main(int argc, const char** argv)
     // run_all_mtl_tests();
     // run_all_ipp_tests();
   
-    test42_load_library_basic();
+    test42_load_library_basic(); // push test into compact
     //test98_motion_blur();
     
     //std::cout << PP_TESTS::test303_median_in_place() << std::endl;
