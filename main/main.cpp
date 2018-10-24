26cf07e9f4571ae041517389f89a92dacadb2885
FROL-HOME\FROL <vova@frolov.pp.ru>
2018-10-24 17:54:28 +0300

    bin2 update

--- main/main.cpp
+++ main/main.cpp
@@ -132,7 +132,7 @@ int main(int argc, const char** argv)
     //demo_01_plane_box();
     //window_main_free_look(L"/home/frol/PROG/HydraAPI/main/tests/test_31", L"opengl1", nullptr, &test02_draw);
     
-    run_all_api_tests();
+    //run_all_api_tests();
 	  run_all_geo_tests();
     run_all_lgt_tests();
     run_all_mtl_tests();
