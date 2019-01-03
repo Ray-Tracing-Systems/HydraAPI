df88e1554c347ce6865fba2d1ddac7d56aef7a77
FROL256 <vova@frolov.pp.ru>
2019-01-03 22:35:54 +0300

    one more invalid chunk bugfix

--- hydra_api/HRMeshCommon.cpp
+++ hydra_api/HRMeshCommon.cpp
@@ -311,10 +311,7 @@ std::shared_ptr<IHRMesh> HydraFactoryCommon::CreateVSGFFromSimpleInputMesh(HRMes
   }
 
   const size_t totalByteSize = totalByteSizeCommon + totalByteSizeCustom;
-
-  const size_t chunkId = g_objManager.scnData.m_vbCache.AllocChunk(totalByteSize, pSysObj->id);
-  auto& chunk          = g_objManager.scnData.m_vbCache.chunk_at(chunkId);
-  chunk.type           = CHUNK_TYPE_VSGF;
+  const size_t chunkId       = g_objManager.scnData.m_vbCache.AllocChunk(totalByteSize, pSysObj->id);
    
   if (chunkId == size_t(-1))
   {
@@ -322,7 +319,10 @@ std::shared_ptr<IHRMesh> HydraFactoryCommon::CreateVSGFFromSimpleInputMesh(HRMes
     return nullptr;
   }
 
+  auto& chunk  = g_objManager.scnData.m_vbCache.chunk_at(chunkId);
+  chunk.type   = CHUNK_TYPE_VSGF;
   char* memory = (char*)chunk.GetMemoryNow();
+
   if (memory == nullptr)
   {
     HrError(L"HydraFactoryCommon::CreateVSGFFromSimpleInputMesh, out of memory unknown error");
