8cc7ad9e9d7ee773ee6f824bca5237a1f4f40bff
frol-msu-white-linux <vova@frolov.pp.ru>
2019-06-19 12:03:06 +0300

    edit

---
+++ main/demo_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+#include <iomanip> // for (std::fixed, std::setfill, std::setw)
+
+#include "../hydra_api/HydraAPI.h"
+#include "../hydra_api/HydraXMLVerify.h"
+
+
+#include "mesh_utils.h"
+
+
