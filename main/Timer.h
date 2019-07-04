3b97c9c14f044ee7acbdff46e82aec8af18a3c9b
vsan-msu <vsan@protonmail.com>
2019-07-04 14:30:12 +0300

    temporary fix (add Timer to /main dir)

---
+++ main/Timer.h
@@ -0,0 +1,65 @@
+
+#ifndef TIMER_GUARDIAN
+#define TIMER_GUARDIAN
+#endif
+
+#if defined WIN32
+
+#include <windows.h>
+#include <stdexcept>
+
+class Timer
+{
+public:
+    explicit        Timer           (bool started = false)  : m_startTicks(-1), m_totalTicks(0) { if (started) start(); }
+                    Timer           (const Timer& other)    { operator=(other); }
+                    ~Timer          (void)                  {}
+
+    Timer&          operator=       (const Timer& other)    { m_startTicks = other.m_startTicks; m_totalTicks = other.m_totalTicks; return *this; }
+
+    void            start           (void)                  { m_startTicks = queryTicks(); }
+    float           getElapsed      (void)                  { return ticksToSecs(getElapsedTicks()); }
+
+private:
+
+    void            unstart(void)   { m_startTicks = -1; }
+    float           end             (void);                 // return elapsed, total += elapsed, restart
+    float           getTotal        (void) const            { return ticksToSecs(m_totalTicks); }
+    void            clearTotal      (void)                  { m_totalTicks = 0; }
+
+private:
+
+    static long long int queryTicks      (void);
+    static float  ticksToSecs     (long long int ticks);
+    long long int getElapsedTicks (void);                 // return time since start, start if unstarted
+
+private:
+    static double s_ticksToSecsCoef;
+    static long long int s_prevTicks;
+
+    long long int   m_startTicks;
+    long long int   m_totalTicks;
+};
+
+#else
+
+#include <sys/time.h>   // for gettimeofday
+
+class Timer
+{
+public:
+    explicit        Timer           (bool started = false)  { m_timeVal = {0,0}; if (started) start(); }
+                    Timer           (const Timer& other)    = default;
+                    ~Timer          ()                      = default;
+    Timer&          operator=       (const Timer& other)    = default;
+    void            start           () { gettimeofday(&m_timeVal, nullptr);}
+    float           getElapsed      ();
+
+private:
+    
+    timeval m_timeVal;
+    
+};
+
+
+#endif
