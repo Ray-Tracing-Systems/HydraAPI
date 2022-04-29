7a0ad836fb8e6db1803154f7bf049a6b16331929
vs-win <vadim.sanzharov@gmail.com>
2022-04-29 18:27:25 +0300

    vector tex wip

---
+++ hydra_api/HRExtensions_VectorTex.h
@@ -0,0 +1,29 @@
+#pragma once
+#define NOMINMAX
+#include <cstdint>
+#include <vector>
+#include <variant>
+
+#include "HydraAPI.h"
+
+namespace hr_vtex
+{
+
+  enum class VTEX_MODE
+  {
+    VTEX_SDF,
+    VTEX_MSDF,
+    VTEX_RASTERIZE
+  };
+
+  struct vtexInfo
+  {
+    VTEX_MODE mode        = VTEX_MODE::VTEX_MSDF;
+    uint32_t  dpi         = 128;   // dots per inch - affects output resolution
+    bool      sdfCombine  = false; // combine all separate shapes sdfs into one texture or create a texture array
+    float     sdfRange    = 4.0f;  //  
+    float     sdfAngThres = 3.0f;  // maximum angle (in radians) to be considered a corner when generating MSDF
+  };
+
+  HAPI HRTextureNodeRef hrTextureVector2DCreateFromFile(const wchar_t* a_fileName, const vtexInfo* a_createInfo, pugi::xml_node* texNode);
+}
